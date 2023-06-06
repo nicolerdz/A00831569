e28f918deade1cf4c1e9e9a603fb3a3072210067
nicolerdz <131408217+nicolerdz@users.noreply.github.com>
2023-06-05 19:12:29 -0600

    Update evidencia_final_uf6_a00831569.py

--- evidencia_final_uf6_a00831569.py
+++ evidencia_final_uf6_a00831569.py
@@ -16,9 +16,8 @@ st.title('Police Incidents Reports from 2018 to 2020 in San Francisco')
 df = pd.read_csv('https://drive.google.com/file/d/1sUXMxryvguQp81yseLxYymTX5RxaH0e9/view?usp=drive_link/Police_Department_Incident_Reports__2018_to_Present.csv')
 
 st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution.')
-# Coordenadas geográficas de San Francisco
 mapa=pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4,
+    np.random.randn(1000, 2) / [50, 50] + [37.7272, -122.4194],
     columns=['lat', 'lon'])
 
 mapa = mapa.dropna()

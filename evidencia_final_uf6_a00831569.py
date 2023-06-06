aa5b3882403bf0fe3c01c02465dd72afbdfa50ff
nicolerdz <131408217+nicolerdz@users.noreply.github.com>
2023-06-05 19:20:49 -0600

    Update evidencia_final_uf6_a00831569.py

--- evidencia_final_uf6_a00831569.py
+++ evidencia_final_uf6_a00831569.py
@@ -13,7 +13,7 @@ import numpy as np
 
 st.title('Police Incidents Reports from 2018 to 2020 in San Francisco')
 
-df = pd.read_csv('https://drive.google.com/file/d/1sUXMxryvguQp81yseLxYymTX5RxaH0e9/view?usp=drive_link/Police_Department_Incident_Reports__2018_to_Present.csv')
+df = pd.read_csv('Police_Department_Incident_Reports__2018_to_Present.csv')
 
 st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution.')
 latitude = df['Latitude']

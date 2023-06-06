1fce364e7d12c6cc35416d8fd56899629ec7eee0
nicolerdz <131408217+nicolerdz@users.noreply.github.com>
2023-06-05 19:39:12 -0600

    Update evidencia_final_uf6_a00831569.py

--- evidencia_final_uf6_a00831569.py
+++ evidencia_final_uf6_a00831569.py
@@ -13,7 +13,18 @@ import pandas as pd
 
 st.title('Police Incidents Reports from 2018 to 2020 in San Francisco')
 
-df = pd.read_csv('mapa.csv')
+df = pd.read_csv('https://drive.google.com/file/d/11oLcKiW8SgCOp3tGiQCYuRG7pLL_J-Zf/view?usp=drive_link')
+
+st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution.')
+# Coordenadas geográficas de San Francisco
+latitude = df['Latitude']
+longitude = df['Longitude']
+
+# Crear un DataFrame con las coordenadas de San Francisco
+mapa = pd.DataFrame(
+    np.array([[latitude, longitude]]),
+    columns=['lat', 'lon']
+)
 
 mapa = mapa.dropna()
 st.map(mapa.astype(float))

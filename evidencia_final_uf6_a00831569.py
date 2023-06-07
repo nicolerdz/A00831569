0394cd5b4d51c586b9cdc1a50e6f4e9848bfaad3
nicolerdz <131408217+nicolerdz@users.noreply.github.com>
2023-06-06 18:35:54 -0600

    Update evidencia_final_uf6_a00831569.py

--- evidencia_final_uf6_a00831569.py
+++ evidencia_final_uf6_a00831569.py
@@ -10,22 +10,27 @@ Original file is located at
 import streamlit as st
 import numpy as np
 import pandas as pd
+import plotly as px
+import plotly.figure_factury as ff
+from brokeh.plotting import figure
+import matplotlib.pyplot as plt
 
 st.title('Police Incidents Reports from 2018 to 2020 in San Francisco')
 
 df = pd.read_csv('https://drive.google.com/file/d/1WQ5QFkP9S1FfpAxCTxQpzWKEP5lD67uL/view?usp=drive_link')
 
 st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution.')
-# Coordenadas geográficas de San Francisco
-latitude = df['Latitude']
-longitude = df['Longitude']
 
-# Crear un DataFrame con las coordenadas de San Francisco
-mapa = pd.DataFrame(
-    np.array([[latitude, longitude]]),
-    columns=['lat', 'lon']
-)
-
+mapa = pd.DataFrame()
+mapa['Date']= df['Incident Date']
+mapa['Day']= df['Incident Day of Week']
+mapa['Police District']= df['Police District']
+mapa['Neighborhood']= df['Analysis Neighborhood']
+mapa['Incident Category']= df['Incidents Subcategory']
+mapa['Resolution'] = df['Resolution']
+mapa['lat'] = df['Latitude']
+mapa['lon'] = df['Longitude']
 mapa = mapa.dropna()
+subset_data2 = mapa
 st.map(mapa.astype(float))
 

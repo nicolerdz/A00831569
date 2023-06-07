0624a1b521c139d5260f17df2d3550847882c445
nicolerdz <131408217+nicolerdz@users.noreply.github.com>
2023-06-06 18:48:30 -0600

    Update evidencia_final_uf6_a00831569.py

--- evidencia_final_uf6_a00831569.py
+++ evidencia_final_uf6_a00831569.py
@@ -17,7 +17,7 @@ import pandas as pd
 st.title('Police Incidents Reports from 2018 to 2020 in San Francisco')
 
 df = pd.read_csv('https://drive.google.com/file/d/1WQ5QFkP9S1FfpAxCTxQpzWKEP5lD67uL/view?usp=drive_link')
-st.DataFrame(df)
+st.dataframe(df)
 st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution.')
 
 mapa = pd.DataFrame()

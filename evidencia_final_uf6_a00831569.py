3ea763f5e597e5ce5dc2889f2d1e01b2db6581b5
nicolerdz <131408217+nicolerdz@users.noreply.github.com>
2023-06-06 18:51:35 -0600

    Update evidencia_final_uf6_a00831569.py

--- evidencia_final_uf6_a00831569.py
+++ evidencia_final_uf6_a00831569.py
@@ -20,7 +20,6 @@ df = pd.read_csv('https://drive.google.com/file/d/1WQ5QFkP9S1FfpAxCTxQpzWKEP5lD6
 st.dataframe(df)
 st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution.')
 
-mapa = pd.DataFrame()
 mapa['Date']= df['Incident Date']
 mapa['Day']= df['Incident Day of Week']
 mapa['Police District']= df['Police District']
@@ -29,8 +28,8 @@ mapa['Incident Category']= df['Incidents Subcategory']
 mapa['Resolution'] = df['Resolution']
 mapa['lat'] = df['Latitude']
 mapa['lon'] = df['Longitude']
-mapa = pd.DataFrame(df['Latitude'],df['Longitude'])
-mapa = mapa.dropna()
+mapa=pd.DataFrame(df['Latitude'], df['Longitude'])
+mapa=mapa.dropna()
 st.map(mapa.astype(int))
 
 
